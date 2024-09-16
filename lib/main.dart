bb5a26e484a0373a4b26f69ed185ff6bfdd76390
auliaahc <auhappycahya11@gmail.com>
2024-09-16 11:13:51 +0700

    `chore`: initialize supabase and ensure flutter bindings

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import "package:dasarata_mobile/env/env.dart";
 import "package:flutter/material.dart";
+import "package:supabase_flutter/supabase_flutter.dart";
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(url: Env.supabaseUrl, anonKey: Env.supabaseKey);
   runApp(const MyApp());
 }
 
