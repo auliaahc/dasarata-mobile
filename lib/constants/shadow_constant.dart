c8c61b1d554612ae0cc09fc2bbe748e9a492980b
auliaahc <auhappycahya11@gmail.com>
2024-09-28 15:49:23 +0700

    `style`: add spacing constant from design system

---
+++ lib/constants/shadow_constant.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ShadowConstant {
+  static List<BoxShadow> shadowSm = [
+    BoxShadow(
+      color: const Color(0XFF000000).withOpacity(0.08),
+      offset: const Offset(0, 4),
+      blurRadius: 19,
+    ),
+  ];
+  static List<BoxShadow> shdowMd = [
+    BoxShadow(
+      color: const Color(0XFF000000).withOpacity(0.1),
+      offset: const Offset(0, 4),
+      blurRadius: 48,
+      spreadRadius: -2,
+    ),
+  ];
+}
