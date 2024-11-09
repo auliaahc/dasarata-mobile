b928ba793545e2c3b4a87ed9ccd661c99d7ba973
auliaahc <auhappycahya11@gmail.com>
2024-11-09 14:41:21 +0700

    `refactor(login)`: change file name

--- lib/screens/login/widgets/submit_button_login_widget.dart
+++ lib/screens/login/widgets/submit_button_login_widget.dart
@@ -3,8 +3,8 @@ import 'package:dasarata_mobile/widgets/button_global_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class ButtonLoginWidget extends StatelessWidget {
-  const ButtonLoginWidget({super.key});
+class SubmitButtonLoginWidget extends StatelessWidget {
+  const SubmitButtonLoginWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
